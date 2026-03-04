22c51374ec90c16f48feb7f1e62479bf5d7c97e6
Jose Victor da Silva Machado <josevictor.sup.shop@alterdata.com.br>
2026-03-04 17:12:51 -0300

    20 Testes automatizados + 1 Em formato Celular

---
+++ cypress/support/commands.js
@@ -0,0 +1,12 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'Poseidon',
+    lastName: 'Enosigaios ',
+    email: 'poseidonenosigaios@hotmail.com',
+    text: 'Teste'
+}) => {
+    cy.get('[name="firstName"]').type(data.firstName)
+    cy.get('[name="lastName"]').type(data.lastName)
+    cy.get(':nth-child(2) > :nth-child(1) > [name="email"]').type(data.email)
+    cy.get('[name="open-text-area"]').type(data.text)
+    cy.get('button[type="submit"]').click()
+})
